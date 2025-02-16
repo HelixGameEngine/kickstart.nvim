72d43f5318c8d455bac4346b79963e502c4350ef
zhaoxiang <zhaoxiang@pm.me>
2025-02-16 23:31:56 +0000

    add few themes

--- init.lua
+++ init.lua
@@ -862,6 +862,17 @@ require('lazy').setup({
   },
 
   {
+    'tiagovla/tokyodark.nvim',
+    opts = {
+      -- custom options here
+    },
+    config = function(_, opts)
+      require('tokyodark').setup(opts) -- calling setup is optional
+      vim.cmd [[colorscheme tokyodark]]
+    end,
+  },
+
+  {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
